b52c90443a200e1e955bbe3cc23a03af96184450
ngalang290898-svg <ngalang290898@gmail.com>
2025-10-29 01:09:42 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,14 +4,31 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['drive.google.com'],
+    domains: ['drive.google.com', 'script.google.com', 'script.googleusercontent.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'drive.google.com',
         pathname: '/uc/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+        pathname: '**',
+      },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ];
   },
   output: 'standalone',
 }
