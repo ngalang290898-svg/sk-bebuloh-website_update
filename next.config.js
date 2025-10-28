6cffb83dbd66e0a2e9985208c67849b7a775a043
ngalang290898-svg <ngalang290898@gmail.com>
2025-10-29 00:32:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['drive.google.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        pathname: '/uc/**',
+      },
+    ],
+  },
+  output: 'standalone',
+}
 
+module.exports = nextConfig
