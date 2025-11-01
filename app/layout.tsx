b4537f41dd4d4524b8335981d4f537d2af7ac2a8
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-01 16:28:06 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter, Poppins } from 'next/font/google'
 import './globals.css'
-import { I18nProvider } from '../lib/i18n'
+import { I18nProvider } from '../lib/i18n' // Fixed path
 
 const inter = Inter({ 
   subsets: ['latin'],
