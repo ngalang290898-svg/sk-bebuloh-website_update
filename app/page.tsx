ac2c17f5d3f3efa4fb0d757aab1fe468ae2e5fea
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-01 15:21:45 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
-// app/page.tsx
-import { redirect } from "next/navigation";
+import HomeLayout from './layouts/HomeLayout'
+import HeroSection from './components/home/HeroSection'
+import WelcomeSection from './components/home/WelcomeSection'
+import VisionMissionSection from './components/home/VisionMissionSection'
+import StaffHighlight from './components/home/StaffHighlight'
+import PKPreviewSection from './components/home/PKPreviewSection'
+import AchievementsSection from './components/home/AchievementsSection'
+import NewsSection from './components/home/NewsSection'
+import ContactSection from './components/home/ContactSection'
 
-export default function Page() {
-  redirect("/en");
+export default function Home() {
+  return (
+    <HomeLayout>
+      <HeroSection />
+      <WelcomeSection />
+      <VisionMissionSection />
+      <StaffHighlight />
+      <PKPreviewSection />
+      <AchievementsSection />
+      <NewsSection />
+      <ContactSection />
+    </HomeLayout>
+  )
 }
